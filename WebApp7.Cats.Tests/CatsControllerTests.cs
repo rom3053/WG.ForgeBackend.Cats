4fbf70366c9103571b9b140e06307dce10317aa3
Роман Джихур <rom3053@gmail.com>
2021-06-10 17:00:21 +0300

    Initial

--- WebApp7.Cats.Tests/CatsControllerTests.cs
+++ WebApp7.Cats.Tests/CatsControllerTests.cs
@@ -37,19 +37,20 @@ namespace WebApp7.Cats.Tests
             {
                 CatsController controller = new CatsController(context);
                 int offset = 0;
-                List<DALL.Cats> movies = controller.SortAsync("tail_length", "asc", 0*10);
+                List<DALL.Cats> movies = new List<DALL.Cats>();
+                //List<DALL.Cats> movies = controller.SortAsync("tail_length", "asc", 0*10);
                 foreach (var item in movies)
                 {
                     outputs.WriteLine(item.ToString());
                 }
                 outputs.WriteLine("------------------");
-                movies = controller.SortAsync("tail_length", "asc", 1*10);
+                //movies = controller.SortAsync("tail_length", "asc", 1*10);
                 foreach (var item in movies)
                 {
                     outputs.WriteLine(item.ToString());
                 }
                 outputs.WriteLine("------------------");
-                movies = controller.SortAsync("tail_length", "asc", 2*10);
+                //movies = controller.SortAsync("tail_length", "asc", 2*10);
                 foreach (var item in movies)
                 {
                     outputs.WriteLine(item.ToString());
